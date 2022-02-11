283d694b23c85b48a64d06ea1bec52ceaa4e8057
Heang Ly <heanglypa@gmail.com>
2022-02-11 08:48:17 -0500

    3 sum

---
+++ 02-Three-Sum.js
@@ -0,0 +1,33 @@
+const threeSum = (nums) => {
+  const target = 0
+  nums.sort((a, b) => a - b)
+  const ans = []
+
+  for (let i = 0; i < nums.length; i++) {
+    let left = i + 1
+    let right = nums.length - 1
+
+    if (i > 0 && nums[i] === nums[i - 1]) continue
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right]
+
+      if (sum === target) {
+        ans.push([nums[i], nums[left], nums[right]])
+
+        while (nums[left] === nums[left + 1]) left++
+        while (nums[right] === nums[right + 1]) right--
+        left++
+        right--
+      } else if (sum > target) {
+        right--
+      } else {
+        left++
+      }
+    }
+  }
+
+  return ans
+}
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]))
